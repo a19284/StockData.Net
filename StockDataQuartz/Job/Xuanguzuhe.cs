1f5e3eebe1f8f758a7c4f18eecf2125659fd2e13
liquanchun <350966707@qq.com>
2017-01-01 14:34:32 +0800

    new code

---
+++ StockDataQuartz/Job/Xuanguzuhe.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Threading;
+using Common.Logging;
+using Quartz;
+using System.Net;
+using HtmlAgilityPack;
+using System.Data;
+using System.Collections.Generic;
+
+namespace StockDataQuartz
+{
+    /// <summary>
+    /// A sample job that just prints info on console for demostration purposes.
+    /// </summary>
+    public class Xuanguzuhe : IJob
+    {
+        private static readonly ILog logger = LogManager.GetLogger(typeof(Xuanguzuhe));
+
+        public void Execute(IJobExecutionContext context)
+        {
+            logger.Info("Start job Xuanguzuhe");
+            try
+            {
+                string sqlstring = string.Format(@"SELECT stock_code,stock_name,count(1) FROM tonghuashunxuangu2
+                                    WHERE record_date = '{0}' and typeid IN (
+		                                    SELECT ID FROM tonghuashunxuangu  WHERE  record_date = '{0}')
+                                    GROUP BY stock_code,stock_name ORDER BY count(1) DESC", DateTime.Today.ToString("yyyy-MM-dd"));
+                DataSet ds = Dbhelper.ExecuteDataset(Dbhelper.Conn, CommandType.Text, sqlstring, null);
+                DataTable data = ds.Tables[0];
+
+                List<string> zuheList = new List<string>();
+                string sqlstring3 = string.Format(@"SELECT * from xuanguzuhe");
+                DataSet ds3 = Dbhelper.ExecuteDataset(Dbhelper.Conn, CommandType.Text, sqlstring3, null);
+                DataTable data3 = ds3.Tables[0];
+                for (int i = 0; i < data3.Rows.Count; i++)
+                {
+                    zuheList.Add(data3.Rows[i]["ids"].ToString());
+                }
+
+                for (int i = 0; i < data.Rows.Count; i++)
+                {
+                    var code = data.Rows[i]["stock_code"].ToString();
+
+                    sqlstring = string.Format(@"SELECT stock_code,stock_name,typeid FROM tonghuashunxuangu2
+                                    WHERE record_date = '{0}' and stock_code='{1}' order by typeid", DateTime.Today.ToString("yyyy-MM-dd"), code);
+                    DataSet ds2 = Dbhelper.ExecuteDataset(Dbhelper.Conn, CommandType.Text, sqlstring, null);
+                    DataTable data2 = ds2.Tables[0];
+
+                    Console.WriteLine(code);
+
+                    List<int> typeidList = new List<int>();
+                    for (int j = 0; j < data2.Rows.Count; j++)
+                    {
+                        typeidList.Add(int.Parse(data2.Rows[j]["typeid"].ToString()));
+                    }
+                    string zuheids = string.Join(",", typeidList.ToArray());
+
+                    if (!zuheList.Contains(zuheids))
+                    {
+                        zuheList.Add(zuheids);
+                        string sqlstring2 = string.Format(@"Insert into xuanguzuhe(name,ids,number)values('选股组合','{0}',{1})", zuheids, typeidList.Count);
+                        Dbhelper.ExecuteNonQuery(Dbhelper.Conn, CommandType.Text, sqlstring2);
+                    }
+
+                    string sqlstring4 = string.Format(@"Insert into xuanguzuhegu(zuheids,stock_code,stock_name,record_date,record_time)values('{0}','{1}','{2}','{3}','{4}')",
+                        zuheids, data2.Rows[0]["stock_code"].ToString(), data2.Rows[0]["stock_name"].ToString(),
+                        DateTime.Today.ToString("yyyy-MM-dd"), DateTime.Now.ToString("HH:mm:ss"));
+                    Dbhelper.ExecuteNonQuery(Dbhelper.Conn, CommandType.Text, sqlstring4);
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.Info("发生错误：" + ex.Message);
+            }
+            logger.Info("End job Xuanguzuhe");
+        }
+    }
+}
--- TestData/Program.cs
+++ TestData/Program.cs
@@ -16,37 +16,41 @@ namespace TestData
     {
         static void Main(string[] args)
         {
-
-            string sqlstring = @"SELECT secondtitle,ID FROM tonghuashunxuangu where record_date='2016-12-30'";
-            DataSet ds = Dbhelper.ExecuteDataset(Dbhelper.Conn, CommandType.Text, sqlstring, null);
-            DataTable data = ds.Tables[0];
-            Dictionary<string, int> dic30 = new Dictionary<string, int>();
-            Parallel.For(0, data.Rows.Count, (i, loopState) =>
-            {
-                dic30.Add(data.Rows[i]["secondtitle"].ToString(), int.Parse(data.Rows[i]["id"].ToString()));
-            });
-
-            sqlstring = @"SELECT secondtitle,ID FROM tonghuashunxuangu where record_date='2016-12-29' or record_date='2016-12-28'";
-            ds = Dbhelper.ExecuteDataset(Dbhelper.Conn, CommandType.Text, sqlstring, null);
-            data = ds.Tables[0];
-            Dictionary<int, int> dic29 = new Dictionary<int, int>();
-            Parallel.For(0, data.Rows.Count, (i, loopState) =>
-            {
-                int id = int.Parse(data.Rows[i]["id"].ToString());
-                if (dic30.ContainsKey(data.Rows[i]["secondtitle"].ToString()))
-                {
-                    dic29.Add(id, dic30[data.Rows[i]["secondtitle"].ToString()]);
-                }
-            });
-
-            Parallel.ForEach(dic29, (item, loopState) =>
-            {
-                sqlstring =string.Format("update tonghuashunxuangu2 set typeid={0} where temptypeid={1}", item.Value,item.Key);
-                Dbhelper.ExecuteNonQuery(Dbhelper.Conn, CommandType.Text, sqlstring);
-            });
-
+            SaveData("http://www.iwencai.com/asyn/search?q=kdj%E9%87%91%E5%8F%89&queryType=stock&app=qnas&qid=");
             Console.WriteLine("The end");
             Console.ReadKey();
+        }
+        static void SaveData(string URLAddress)
+        {
+            WebClient client = new WebClient();
+            client.Encoding = System.Text.Encoding.GetEncoding("UTF-8");
+            try
+            {
+                string html = client.DownloadString(URLAddress);
+                var serializer = new JavaScriptSerializer();
+                Dictionary<string, object> data = (Dictionary<string, object>)serializer.Deserialize(html, typeof(object));
+                Dictionary<string, object> suggest =(Dictionary<string, object>)data["suggest"];
+                
+                foreach (var item in suggest)
+                {
+                    string value = item.Value.ToString();
+                    HtmlAgilityPack.HtmlDocument htmlDoc = new HtmlAgilityPack.HtmlDocument();
+
+                    htmlDoc.LoadHtml(value);
+                    HtmlNode Node1 = htmlDoc.DocumentNode;
+                    var linodes = Node1.SelectSingleNode("child::ul[1]").SelectNodes("child::li");
+                    for (int i = 1; i < linodes.Count; i++)
+                    {
+                        var lititle = linodes[i].SelectSingleNode("child::a[1]").Attributes["title"].Value;
+                        var url = linodes[i].SelectSingleNode("child::a[1]").Attributes["href"].Value;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Zhinengxuangu1," + URLAddress);
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
